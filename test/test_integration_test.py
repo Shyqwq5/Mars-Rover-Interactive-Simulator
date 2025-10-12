9705b99ca4180d575ce95d1037b8e39d7c3cb003
Shyqwq5 <shyqwq5@gmail.com>
2025-10-12 20:21:17 +0100

    feat: introduce inegration test

--- test/test_integration_test.py
+++ test/test_integration_test.py
@@ -1,11 +1,11 @@
 from src.logic_layer.mission_control import MissionControl
-from src.logic_layer.plateau import Plateau
-from src.instruction import Instruction
 from src.compass_direction import CompassDirection
 from src.input_layer.instruction_parser import InstructionParse
 from src.input_layer.land_position_parser import LandPositionParser
 from src.input_layer.plateau_parser import PlateauParser
-import pytest
+# from src.logic_layer.plateau import Plateau
+# from src.instruction import Instruction
+# import pytest
 
 
 def test_integration_input_and_mission_control():
@@ -24,8 +24,6 @@ def test_integration_input_and_mission_control():
     parsed_landposition_2 =  LandPositionParser().parse_land_position('3 3 E')
     parsed_instruction_2 = InstructionParse().parse_instruction('MMRMMRMRRM')
     new_mission.add_rover(parsed_landposition_2)
-    new_mission.move_latest_rover(parsed_instruction_2)
-    assert new_mission.rovers[next(reversed(new_mission.rovers))].direction == CompassDirection.EAST
-    assert next(reversed(new_mission.rovers))== (5,1)
-
+    result = new_mission.move_latest_rover(parsed_instruction_2)
+    assert result == [5,1,CompassDirection.EAST]
 
