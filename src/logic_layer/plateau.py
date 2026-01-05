821a4750043c8490ff9c9365c9da623c864c7d2b
Shyqwq5 <shyqwq5@gmail.com>
2026-01-05 22:14:39 +0000

    Refine plateau boundaries and movement constraints

--- src/logic_layer/plateau.py
+++ src/logic_layer/plateau.py
@@ -13,7 +13,7 @@ class Plateau:
                 map[(i,j)] = None
         return map
 
-    def creat_rough_map(self,percentage = 0.2):
+    def creat_rough_map(self,percentage = 0.1):
         map = {}
         for i in range(self.latitdude+1):
             for j in range(self.lontitude+1):
